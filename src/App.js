44a6d70445f21126dd703b3d3146670514d38dfa
jerome11sgic <jerome11sgic@gmail.com>
2020-05-29 23:34:00 +0530

    First Layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Topbar from "./layout/Topbar";
+import SideBar from "./layout/SideBar";
+import Footer from "./layout/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <SideBar />
+
+      <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content">
+          <Topbar />
+          <div class="container-fluid">
+            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+              <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+              <a
+                href="#"
+                class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"
+              >
+                <i class="fas fa-download fa-sm text-white-50"></i> Generate
+                Report
+              </a>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
